2d2162ca6f855de54f4a05f6f553a45a808fe6c3
Roman Kolisnyk <romkolisnyk@gmail.com>
2021-10-05 21:09:36 +0300

    move 404 styles to global lvl + change import order

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,12 @@
-import MainPage from '../main-page/main-page';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import MainPage from '../main-page/main-page';
 import NotFoundPage from '../not-found-page/not-found-page';
 import FavoritesPage from '../favorites-page/favorites-page';
 import LoginPage from '../login-page/login-page';
 import PropertyPage from '../property-page/property-page';
-import { AppRoute, AuthorizationStatus } from '../../const';
 import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppScreenProps = {
   offersCount: number;
--- project/src/components/not-found-page/not-found-page.tsx
+++ project/src/components/not-found-page/not-found-page.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import './styles.css';
 
 function NotFoundPage(): JSX.Element {
   return (
