0d80f8a83f0b0ca64d7530dc683bfae30720c352
Roman Kolisnyk <romkolisnyk@gmail.com>
2021-10-25 19:13:28 +0300

    fix: Change action names

--- project/src/types/action.ts
+++ project/src/types/action.ts
@@ -1,8 +1,8 @@
 import { changeCity, loadOffers } from '../store/action';
 
 export enum ActionType {
-  ChangeCity = 'offers/changeCity',
-  LoadOffers = 'offers/loadOffers',
+  ChangeCity = 'city/change',
+  LoadOffers = 'offers/load',
 }
 
 export type Actions = ReturnType<typeof changeCity> | ReturnType<typeof loadOffers>;
