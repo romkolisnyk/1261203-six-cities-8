842550d629030c1d632b300d32cdc15fdabb970a
Roman Kolisnyk <romkolisnyk@gmail.com>
2021-11-01 23:40:04 +0200

    feat: Configure api

---
+++ project/src/api/api.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios';
+
+const BACKEND_URL = 'https://8.react.pages.academy/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+enum HttpCode {
+  Unauthorized = 401,
+};
+
+type UnauthorizedCallback = () => void;
+
+export const createAPI = (onUnauthorized: UnauthorizedCallback): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const { response } = error;
+
+      if (response?.status === HttpCode.Unauthorized) {
+        return onUnauthorized();
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+};
